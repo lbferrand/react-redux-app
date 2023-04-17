be959645ca536dda2159431297aa368cc19162e9
lbferrand <lbferrand@gmail.com>
2023-04-17 11:06:25 -0400

    Add props for dynamic setup

--- src/index.js
+++ src/index.js
@@ -3,32 +3,40 @@ import ReactDOM from "react-dom/client";
 
 import './index.css';
 
+const author = 'Lucy Score';
+const title = 'Things We Never Got Over';
+const img = './images/book1.jpg';
+
+// //parameters
+// const someFunc = (param1, param2) => {
+//   console.log(param1, param2);
+// };
+// //arguments
+// someFunc('job', 'developer');
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={img}/>
+      <Book author={author} title={title} img={img}/>
     </section>
   )
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
+  
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src={props.img} alt={props.title}/>
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+
     </article>
   )
 }
 
-const Image = () => <img src='./images/book1.jpg' alt="Things We Never Got Over"/>;
-const Title = () => {
-  return <h2>Things We Never Got Over</h2>
-};
-const Author = () => <h4 style={{color:'#617d98', fontSize:'0.75rem', marginTop:'0.5rem'}}>Lucy Score</h4>
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 

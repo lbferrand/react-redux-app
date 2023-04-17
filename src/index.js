f9df093d704cad3cf1421b59b503b66cdb7ad2f6
lbferrand <lbferrand@gmail.com>
2023-04-17 14:21:20 -0400

    Add images

--- src/index.js
+++ src/index.js
@@ -3,10 +3,47 @@ import ReactDOM from "react-dom/client";
 
 import './index.css';
 
-const author = 'Lucy Score';
-const title = 'Things We Never Got Over';
-const img = './images/book1.jpg';
+const book1 = {
+  author: 'Lucy Score',
+  title: 'Things We Never Got Over',
+  img: './images/book1.jpg',
+}
 
+const book2 = {
+  author: 'Charlie Donlea',
+  title: 'Twenty Years Later',
+  img: './images/book2.jpg',
+};
+
+const book3 = {
+  author: 'Mary Stone',
+  title: "Shadow's Secret",
+  img: './images/book3.jpg',
+};
+
+const book4 = {
+  author: 'Lisa Jackson',
+  title: "The girl who survived",
+  img: './images/book4.jpg',
+};
+
+const book5 = {
+  author: 'Valerie Keogh',
+  title: "The Widow",
+  img: './images/book5.jpg',
+}
+
+const book6 = {
+  author: 'Keri Beevis',
+  title: "The Summer House",
+  img: './images/book6.jpg',
+}
+
+const book7 = {
+  author: 'L.H. Stacey',
+  title: 'The Weekend',
+  img: './images/book7.jpg',
+}
 // //parameters
 // const someFunc = (param1, param2) => {
 //   console.log(param1, param2);
